35b651f2b62ba30b66cb2cc8865b16efa695a4a3
carframo2 <97231037+carframo2@users.noreply.github.com>
2026-05-04 14:07:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-API_TOKEN = os.getenv("API_TOKEN", "changeme")
+API_TOKEN = os.getenv("BRIDGE_TOKEN", "changeme")
 jobs = {}
 lock = Lock()
 
